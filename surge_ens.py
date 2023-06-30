e8ee102b25a03ae7ea226c612f3e9a4c67693283
jpolton <jpolton@gmail.com>
2023-06-30 14:52:01 +0100

    fix: get latest file method

--- surge_ens.py
+++ surge_ens.py
@@ -15,7 +15,7 @@ import matplotlib.dates as mdates
 from matplotlib.dates import DateFormatter
 from matplotlib.cbook import get_sample_data
 
-import glob
+import glob, os
 import numpy as np
 import xarray as xr
 from socket import gethostname
