4897abca9ec4bc713291137ad082a9fb328981da
jpolton <jelt@noc.ac.uk>
2024-10-09 14:21:47 +0100

    Update run_surge_anim.sh

--- run_surge_anim.sh
+++ run_surge_anim.sh
@@ -5,21 +5,15 @@
 # 30 19 * * * /login/jelt/GitHub/NTSLF_code/run_surge_anim.sh
 # 30 07 * * * /login/jelt/GitHub/NTSLF_code/run_surge_anim.sh
 
+# Add process for generating ensemble surge plot for tidegauge network
+cd /login/surges/matlab/; module load matlab; matlab -nodesktop -nosplash -r autoplot_latestensemble;
+# Expected output: /projectsa/surge_archive/figures/ensembles_latest/EnsembleClassAOffset_res_latest.png
+# Gets moved to FTP server with the surge_ens.py script
+
 source /packages/lmodmodules/apps/anaconda/5-2021/bin/activate /work/jelt/conda-env/ntslf_py39
 source /etc/profile.d/modules.sh
 #python Documents/my_python_file_name.py WRONG. SEPARATLY GO TO FOLDER THEN EXECUTE PYTHON
 cd /login/jelt/GitHub/NTSLF_code/  
 python surge_anim.py > surge_anim.log
 python surge_ens.py > surge_ens.log
-conda deactivate
-
-# Add process for generating ensemble surge plot for tidegauge network
-cd /login/surges/matlab/; module load matlab; matlab -nodesktop -nosplash -r autoplot_latestensemble;
-# Expected output: /projectsa/surge_archive/figures/ensembles_latest/EnsembleClassAOffset_res_latest.png
-
-# Add python script to move file to ftp server
-source /packages/lmodmodules/apps/anaconda/5-2021/bin/activate /work/jelt/conda-env/ntslf_py39
-source /etc/profile.d/modules.sh
-cd /login/jelt/GitHub/NTSLF_code/  
-python mv_plot_to_ftpserver.py > mv_plot_to_ftpserver.log
 conda deactivate
