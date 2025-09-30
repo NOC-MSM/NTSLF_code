2376be149c0531d2e8d8c39d4c2ddda20cf23462
jpolton <jpolton@users.noreply.github.com>
2025-09-30 13:46:46 +0100

    update paths

--- run_surge_anim.sh
+++ run_surge_anim.sh
@@ -5,13 +5,17 @@
 # 30 19 * * * /login/jelt/GitHub/NTSLF_code/run_surge_anim.sh
 # 30 07 * * * /login/jelt/GitHub/NTSLF_code/run_surge_anim.sh
 
+
+source /lmod/modules/apps/anaconda/2023.03/bin/activate /work/jelt/conda-env/ntslf_py39
+source /etc/profile.d/00-modulepath.sh
+
+
 # Add process for generating ensemble surge plot for tidegauge network
 cd /login/surges/matlab/; module load matlab; matlab -nodesktop -nosplash -r autoplot_latestensemble;
 # Expected output: /projectsa/surge_archive/figures/ensembles_latest/EnsembleClassAOffset_res_latest.png
 # Gets moved to FTP server with the surge_ens.py script
 
-source /packages/lmodmodules/apps/anaconda/5-2021/bin/activate /work/jelt/conda-env/ntslf_py39
-source /etc/profile.d/modules.sh
+
 #python Documents/my_python_file_name.py WRONG. SEPARATLY GO TO FOLDER THEN EXECUTE PYTHON
 cd /login/jelt/GitHub/NTSLF_code/  
 python surge_anim.py > surge_anim.log
